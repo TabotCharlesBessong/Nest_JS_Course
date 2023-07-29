9d00acb81550b3cf5e2405f7c14c5a1b7681c414
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-07-29 17:46:44 +0100

    feat: ( created the signup service function)

--- nest-js-api-tutorial/src/auth/auth.service.ts
+++ nest-js-api-tutorial/src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import {PrismaService} from '../prisma/prisma.service'
 import { AuthDto } from "./dto";
+import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService {
@@ -9,7 +11,34 @@ export class AuthService {
     return { msg: 'I am signin' };
   }
 
-  signup(dto: AuthDto) {
-    return { msg: 'I am signup' };
+  async signup(dto: AuthDto) {
+    // generate the hashed password
+    const hash = await argon.hash(dto.password);
+    // save the user to the database
+    try {
+      const user = await this.prisma.user.create({
+        data:{
+          email:dto.email,
+          hash
+        },
+        // an alternative way of not returning the user password
+        // select:{
+        //   id:true,
+        //   email:true,
+        //   createdAt:true
+        // }
+      })
+  
+      delete user.hash
+      return {user};
+      
+    } catch (error) {
+      if(error instanceof PrismaClientKnownRequestError){
+        if(error.code === 'P2002'){
+          throw new ForbiddenException('credidentials taken')
+        }
+      }
+      throw error
+    }
   }
 }
