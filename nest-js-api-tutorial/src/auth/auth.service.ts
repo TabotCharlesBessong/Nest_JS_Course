c00f586b999d6d74f228b50c7560c015f5ee7c85
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-07-29 16:41:19 +0100

    dto pipeline validation

--- nest-js-api-tutorial/src/auth/auth.service.ts
+++ nest-js-api-tutorial/src/auth/auth.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import {PrismaService} from '../prisma/prisma.service'
+import { AuthDto } from "./dto";
 
 @Injectable()
-export class AuthService{
-  constructor(private prisma: PrismaService){}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signin() {
-    return { msg: 'I am signup' };
+    return { msg: 'I am signin' };
   }
 
-  signup() {
-    return { msg: 'I am signin' };
+  signup(dto: AuthDto) {
+    return { msg: 'I am signup' };
   }
 }
