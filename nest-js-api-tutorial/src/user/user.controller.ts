d9dd3465b875bbde7c074a94662ed5c68cfa1147
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-07-30 11:59:57 +0100

    dynamic decorator and guard

--- nest-js-api-tutorial/src/user/user.controller.ts
+++ nest-js-api-tutorial/src/user/user.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe(@Req() req: Request) {
-    console.log({
-      user:req.user
-    })
-    return req.user
+  getMe(@GetUser() user:User ) {
+    // console.log({
+    //   user:req.user
+    // })
+    return user
   }
 }
