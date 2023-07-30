fcb137da3b46ec2008fdd460d8e6e9b4f1812acc
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-07-30 07:34:50 +0100

    hidding the vulnerable inforlations  with config

--- nest-js-api-tutorial/dist/app.module.js
+++ nest-js-api-tutorial/dist/app.module.js
@@ -12,11 +12,20 @@ const auth_module_1 = require("./auth/auth.module");
 const bookmark_module_1 = require("./bookmark/bookmark.module");
 const user_module_1 = require("./user/user.module");
 const prisma_module_1 = require("./prisma/prisma.module");
+const config_1 = require("@nestjs/config");
 let AppModule = exports.AppModule = class AppModule {
 };
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [auth_module_1.AuthModule, user_module_1.UserModule, bookmark_module_1.BookmarkModule, prisma_module_1.PrismaModule],
+        imports: [
+            config_1.ConfigModule.forRoot({
+                isGlobal: true
+            }),
+            auth_module_1.AuthModule,
+            user_module_1.UserModule,
+            bookmark_module_1.BookmarkModule,
+            prisma_module_1.PrismaModule,
+        ],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
--- nest-js-api-tutorial/src/prisma/prisma.service.ts
+++ nest-js-api-tutorial/src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor(){
+  constructor(config:ConfigService){
     super({
       datasources: {
         db: {
-          url: 'mongodb+srv://charles-junior:junior1967@cluster0.pazzesj.mongodb.net/nest_api_tutorial',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
