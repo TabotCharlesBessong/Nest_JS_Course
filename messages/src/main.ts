9208e9b4ff6324ca8d97354fc665cd34f39ea01e
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-18 01:18:32 +0100

    reference code

---
+++ messages/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { MessagesModule } from './messages/messages.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessagesModule);
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();
