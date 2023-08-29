48d254f86924dcbdcd99a320cc719577677de9d9
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-29 23:10:07 +0100

    saving user data to the sqlite database

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,10 +1,12 @@
+import { UsersService } from './users.service';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService:UsersService){}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body)
+    this.usersService.create(body.email,body.password)
   }
 }
