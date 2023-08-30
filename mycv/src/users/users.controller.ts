77925c9cafa59084d5e14058aee432c7be92ec70
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-30 14:11:30 +0100

    deleting user

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { UsersService } from './users.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
@@ -8,5 +8,20 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email,body.password)
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id :string){
+    return this.usersService.findOne(parseInt(id))
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email:string){
+    return this.usersService.find(email)
+  }
+
+  @Delete(':/id')
+  removeUser(@Param('id') id:string){
+    return this.usersService.remove(parseInt(id))
   }
 }
