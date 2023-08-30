e9ed7903f0431c9e1b1779895760e9236087873c
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-30 12:10:54 +0100

    user services with find, update and remove

--- mycv/src/users/users.service.ts
+++ mycv/src/users/users.service.ts
@@ -5,12 +5,38 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private repo:Repository<User>) {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
   }
 
-  create(email:string,password:string) {
-    const user = this.repo.create({email,password})
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
 
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id:number,attrs:Partial<User>) {
+    const user = await this.findOne(id)
+    if(!user){
+      throw new Error('user not found')
+    }
+    Object.assign(user,attrs)
     return this.repo.save(user)
+  }
+
+  async remove(id:number) {
+    const user = await this.findOne(id)
+
+    if(!user) {
+      throw new Error('user does not exist')
+    }
+
+    return this.repo.remove(user)
   }
 }
