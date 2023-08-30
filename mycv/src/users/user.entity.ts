3c62d318b1baae39812a48596d010341ab94f8a1
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-30 10:36:36 +0100

    exploring the difference between save and create

--- mycv/src/users/user.entity.ts
+++ mycv/src/users/user.entity.ts
@@ -1,5 +1,5 @@
 
-import { Entity,Column,PrimaryGeneratedColumn } from "typeorm";
+import { Entity,Column,PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from "typeorm";
 
 @Entity()
 export class User {
@@ -11,4 +11,19 @@ export class User {
 
   @Column()
   password:string
+
+  @AfterInsert()
+  logInsert(){
+    console.log('inserted user with id ',this.id)
+  }
+
+  @AfterUpdate()
+  logUpdate(){
+    console.log('updated User with id ',this.id)
+  }
+
+  @AfterRemove()
+  logRemove(){
+    console.log('removed user with id ',this.id)
+  }
 }
