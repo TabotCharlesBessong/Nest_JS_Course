5e41e8d069d600e3f54109263f7cc86ad54caa7d
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-31 10:21:00 +0100

    interceptors dto

--- mycv/src/interceptors/serialize.interceptors.ts
+++ mycv/src/interceptors/serialize.interceptors.ts
@@ -1,15 +1,17 @@
-import { CallHandler, ExecutionContext, NestInterceptor, Next } from "@nestjs/common";
-import { Observable, map } from "rxjs";
+import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { UserDto } from "src/users/dtos/user-dto";
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context:ExecutionContext,handler:CallHandler):Observable<any>{
-    // run something before a request is followed bya request handler
-    console.log('running before request handler')
 
     return handler.handle().pipe(
       map((data:any) => {
-        // run something before the response is sent out
-        console.log('am running before the response is sent',data)
+        return plainToClass(UserDto,data,{
+          excludeExtraneousValues:true
+        })
       })
     )
   }
