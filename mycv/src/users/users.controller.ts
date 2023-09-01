aa3c345666b575a8b93aebbcd61773c87ba3558b
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-01 22:08:31 +0100

    interceptors and decorators

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -17,6 +17,8 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user-dto';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { CurrentUser } from './decorators/current-user.decorators';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -26,9 +28,14 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  // @Get('/whoami')
+  // whoAmi(@Session() session:any){
+  //   return this.usersService.findOne(session.userId)
+  // }
+
   @Get('/whoami')
-  whoAmi(@Session() session:any){
-    return this.usersService.findOne(session.userId)
+  whoAmI(@CurrentUser() user:string){
+    return user
   }
 
   @Post('/signout')
