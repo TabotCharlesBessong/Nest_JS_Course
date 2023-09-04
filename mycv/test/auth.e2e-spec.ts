7dae23cb8c764223501a5caf077da7c6421fe082
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-05 00:36:56 +0100

    end to end integrated testing

---
+++ mycv/test/auth.e2e-spec.ts
@@ -0,0 +1,34 @@
+
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const email = 'asdjksde@sgh.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password:'qerffdshj;'
+      })
+      .expect(201)
+      .then((res) => {
+        const {id,email} = res.body
+        expect(id).toBeDefined()
+        expect(email).toEqual(email);
+      })
+  });
+});
