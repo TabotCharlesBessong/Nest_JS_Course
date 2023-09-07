5e29929139ea6f0705297f17f0e6adcd13b1fdfd
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-07 13:28:53 +0100

    rounding up with testing

--- mycv/test/auth.e2e-spec.ts
+++ mycv/test/auth.e2e-spec.ts
@@ -1,4 +1,3 @@
-
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
@@ -22,13 +21,33 @@ describe('Authentication System', () => {
       .post('/auth/signup')
       .send({
         email,
-        password:'qerffdshj;'
+        password: 'qerffdshj;',
       })
       .expect(201)
       .then((res) => {
-        const {id,email} = res.body
-        expect(id).toBeDefined()
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
         expect(email).toEqual(email);
+      });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'glen.tenjoh@ub.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: 'abcdef',
       })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const {body} = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie).expect(200)
+    
+    expect(body.email).toEqual(email)
   });
 });
