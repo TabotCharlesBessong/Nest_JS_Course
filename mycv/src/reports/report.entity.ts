bf01683fb35296f03a944bd75c12788d4be5bbdd
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-08 17:11:21 +0100

    establishing a one to many relationship

--- mycv/src/reports/report.entity.ts
+++ mycv/src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
 @Entity()
 export class Report {
@@ -25,4 +26,7 @@ export class Report {
 
   @Column()
   mileage: number
+
+  @ManyToOne(() => User, (user) => user.reports)
+  user:User
 }
--- mycv/src/users/user.entity.ts
+++ mycv/src/users/user.entity.ts
@@ -1,3 +1,4 @@
+import { Report } from 'src/reports/report.entity';
 import {
   AfterInsert,
   AfterRemove,
@@ -5,6 +6,7 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
+  OneToMany,
 } from 'typeorm';
 
 @Entity()
@@ -17,6 +19,9 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports:Report[]
 
   @AfterInsert()
   logInsert() {
