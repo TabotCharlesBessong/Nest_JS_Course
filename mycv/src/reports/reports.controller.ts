52d641ccfa9a99b3dac61417785ff42ca4c5a8ee
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-08 16:49:51 +0100

    setting up report and creation of reports

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+
+  constructor(private reportService:ReportsService)
+{}
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto){
+    return this.reportService.create(body)
+  }
+}
