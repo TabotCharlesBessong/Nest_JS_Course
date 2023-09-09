c8be86a8708f4c397fb863b376e0cde984b652d0
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-09 02:30:53 +0100

    integrating the guard into our application

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -7,6 +7,7 @@ import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApprovedReportDto } from './dtos/approved-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('reports')
 export class ReportsController {
@@ -21,6 +22,7 @@ export class ReportsController {
   }
 
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approvedReport(@Param('id') id : string, @Body() body: ApprovedReportDto){
     return this.reportService.changeApproval(id,body.approved)
   }
