c13a0230cd7bb0510e197609374f0f2fc42f75d8
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-09 01:19:55 +0100

    hidding sensitive informations

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -4,6 +4,8 @@ import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -12,6 +14,7 @@ export class ReportsController {
 {}
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User){
     return this.reportService.create(body,user)
   }
