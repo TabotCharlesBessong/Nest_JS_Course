745cf74311b476cfee17dde1fa5394da8d579cc0
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-09 01:41:51 +0100

    patch request to approve a car report

--- mycv/src/reports/report.entity.ts
+++ mycv/src/reports/report.entity.ts
@@ -27,6 +27,9 @@ export class Report {
   @Column()
   mileage: number
 
+  @Column({default:false})
+  approved:boolean
+
   @ManyToOne(() => User, (user) => user.reports)
   user:User
 }
--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -6,6 +6,7 @@ import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
+import { ApprovedReportDto } from './dtos/approved-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -17,5 +18,10 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User){
     return this.reportService.create(body,user)
+  }
+
+  @Patch('/:id')
+  approvedReport(@Param('id') id : string, @Body() body: ApprovedReportDto){
+    return this.reportService.changeApproval(id,body.approved)
   }
 }
