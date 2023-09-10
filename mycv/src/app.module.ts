5cafba4140f1461a6ce52af7d50fc1f730825c38
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-10 21:10:24 +0100

    a new env

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -48,11 +48,14 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(
+    private configService:ConfigService
+  ){}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['asdfasfd'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
