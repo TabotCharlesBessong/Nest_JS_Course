cd284c71083baec6b016713ab1709e62b19cd3b0
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-10 17:46:19 +0100

    transforming querystring into appropriate datatype

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -8,6 +8,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApprovedReportDto } from './dtos/approved-report.dto';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -25,5 +26,10 @@ export class ReportsController {
   @UseGuards(AdminGuard)
   approvedReport(@Param('id') id : string, @Body() body: ApprovedReportDto){
     return this.reportService.changeApproval(id,body.approved)
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto){
+    console.log(query)
   }
 }
