addc139b7f16f4a0edb713b79cbfa1bbb1448dd7
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-10 14:14:40 +0100

    applying current user middleware

--- mycv/src/users/users.module.ts
+++ mycv/src/users/users.module.ts
@@ -1,22 +1,21 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthService } from './auth.service';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { AuthService } from './auth.service';
-import { User } from './user.entity';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [
     UsersService,
-    AuthService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    },
+    AuthService
   ],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer:MiddlewareConsumer){
+    consumer.apply(CurrentUserMiddleware).forRoutes('*')
+  }
+}
