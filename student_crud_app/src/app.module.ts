57be4da080c0b6436639e996392ad1b17e1eb7f3
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-29 15:28:09 +0100

    setting up database connection

--- student_crud_app/src/app.module.ts
+++ student_crud_app/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      '',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
