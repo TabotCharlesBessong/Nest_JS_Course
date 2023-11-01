0d21164558775e8bb03cf8e7a811e7d8db41edf9
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-01 08:49:31 +0100

    create and update user dto for validation

--- student_crud_app/dist/app.module.js
+++ student_crud_app/dist/app.module.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const mongoose_1 = require("@nestjs/mongoose");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 let AppModule = class AppModule {
@@ -15,7 +16,9 @@ let AppModule = class AppModule {
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            mongoose_1.MongooseModule.forRoot('mongodb+srv://tabotcharles:charles1974@cluster0.vjfxkls.mongodb.net/student_crud'),
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
