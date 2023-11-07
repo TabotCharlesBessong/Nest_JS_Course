14166d58b1f2bafc1f8fab024d376d2c649a1b0d
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-07 17:07:00 +0100

    established a connection to the database

--- employee_management_system/src/app.module.ts
+++ employee_management_system/src/app.module.ts
@@ -1,9 +1,22 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
