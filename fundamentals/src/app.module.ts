a7534505a4a70c8e3dae3f1ad6f93b4dd99cc79b
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-04-04 15:17:12 +0100

    song modules

--- fundamentals/src/app.module.ts
+++ fundamentals/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SongsModule } from './songs/songs.module';
 
 @Module({
-  imports: [],
+  imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
