5b4007c701fa7b12045163784c50a15e820f7560
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-04-04 17:31:39 +0100

    class validators

--- spotify_server/src/songs/songs.controller.ts
+++ spotify_server/src/songs/songs.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from "@nestjs/common"
 import { SongsService } from './songs.service';
+import { CreateSongDTO } from "./dto/create-song-dto";
 
 @Controller('songs')
 export class SongsController {
   constructor(private songsService:SongsService){}
   @Post()
-  create(){
-    return this.songsService.create("hi there")
+  create(@Body() createSongDTO:CreateSongDTO){
+    return this.songsService.create(createSongDTO)
   }
 
   @Get()
