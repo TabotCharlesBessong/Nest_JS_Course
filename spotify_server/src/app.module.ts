389d8a05ff7565a9ae43f8851f0ae2e89a718422
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-04-05 09:17:06 +0100

    middleares (logger functions)

--- spotify_server/src/app.module.ts
+++ spotify_server/src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes("songs")
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'songs', method: RequestMethod.POST });
+  }
+}
